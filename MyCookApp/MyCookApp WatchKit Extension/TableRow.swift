ff0d9191ea27626f2930b5ffb252ca5142c9e3ce
Nember1507 <53421389+Nember1507@users.noreply.github.com>
2022-04-06 10:32:43 +0900

    Add files via upload

---
+++ MyCookApp/MyCookApp WatchKit Extension/TableRow.swift
@@ -0,0 +1,16 @@
+//
+//  TableRow.swift
+//  MyCookApp WatchKit Extension
+//
+//  Created by Efremov on 04.04.2022.
+//
+
+import WatchKit
+import Foundation
+
+class TableRow: NSObject {
+    
+    @IBOutlet weak var recipeNameLabel: WKInterfaceLabel!
+    @IBOutlet weak var recipeIconLabel: WKInterfaceLabel!
+    
+}
